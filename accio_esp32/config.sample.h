0b5a95f073628dbabaa5f1ad33e504229c3f3070
Ariful Islam <ariful@portonics.com>
2019-10-25 11:35:53 +0600

    esp32 arduino script

---
+++ accio_esp32/config.sample.h
@@ -0,0 +1,3 @@
+#define WIFI_SSID = "YOUR_WIFI_NAME";
+#define WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+#define MOTION_NOTIFY_URL = "http://your-url.com/motion?id=0";
---
+++ shield-shutdown.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+/opt/bin/adb connect 192.168.1.170
+/opt/bin/adb connect 192.168.1.170
+/opt/bin/adb shell reboot -p
